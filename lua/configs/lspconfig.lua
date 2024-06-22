8ffb139b688a3f519ed72945da909222578e3b55
Beefysam211 <samarth.shammy.goel@gmail.com>
2024-06-22 17:10:23 +0530

    config tweaks

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,7 +3,21 @@ local on_init = require("configs.lspconfig").on_init
 local capabilities = require("configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "tsserver", "bashls", "clangd", "vimls", "lua_ls", "zls" }
+
+lspconfig.clangd.setup {
+  on_attach = function(client, bufnr)
+    on_attach(client, bufnr)
+    if client.name == "clangd" and client.resolved_capabilities then
+      client.resolved_capabilities.document_formatting = true
+    end
+  end,
+  on_init = on_init,
+  capabilities = capabilities,
+  flags = {
+    debounce_text_changes = 150,
+  },
+}
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
